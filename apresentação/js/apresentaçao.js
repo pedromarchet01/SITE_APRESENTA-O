40a3c7cd5021b94c66ca660cb7a9921515b451c5
PEDRO MARCHET <100133140+pedromarchet01@users.noreply.github.com>
2022-06-17 21:26:24 -0300

    Add files via upload

---
+++ apresentação/js/apresentaçao.js
@@ -0,0 +1,216 @@
+var link1 = document.querySelector(".link1");
+
+link1.style.backgroundColor = "navy";
+link1.style.boxShadow = "3px 3px 3px black";
+link1.style.padding = "5px";
+link1.style.textAlign = "justify";
+link1.style.color = "white";
+link1.style.textShadow = " 1px 1px 3px black";
+link1.style.border = "3px solid navy";
+link1.style.borderRadius = "5px";
+link1.style.fontSize = "1.9vw";
+link1.style.fontWeight = "bold";
+
+link1.addEventListener("mouseover", function() {
+  this.style.backgroundColor = "orange";
+  this.style.border = "3px solid orange";
+  this.style.borderRadius = "20px";
+  this.style.transition = "1s";
+});
+
+link1.addEventListener("mouseout", function() {
+  this.style.backgroundColor = "navy";
+  this.style.border = "3px solid navy";
+  this.style.borderRadius = "5px";
+  this.style.transition = "1s";
+});
+
+var link2 = document.querySelector(".link2");
+
+link2.style.backgroundColor = "navy";
+link2.style.boxShadow = "3px 3px 3px black";
+link2.style.padding = "5px";
+link2.style.textAlign = "justify";
+link2.style.color = "white";
+link2.style.textShadow = " 1px 1px 3px black";
+link2.style.border = "3px solid navy";
+link2.style.borderRadius = "5px";
+link2.style.fontSize = "1.9vw";
+link2.style.fontWeight = "bold";
+
+link2.addEventListener("mouseover", function() {
+  this.style.backgroundColor = "orange";
+  this.style.border = "3px solid orange";
+  this.style.borderRadius = "20px";
+  this.style.transition = "1s";
+});
+
+link2.addEventListener("mouseout", function() {
+  this.style.backgroundColor = "navy";
+  this.style.border = "3px solid navy";
+  this.style.borderRadius = "5px";
+  this.style.transition = "1s";
+});
+
+var link3 = document.querySelector(".link3");
+
+link3.style.backgroundColor = "navy";
+link3.style.boxShadow = "3px 3px 3px black";
+link3.style.padding = "5px";
+link3.style.textAlign = "justify";
+link3.style.color = "white";
+link3.style.textShadow = " 1px 1px 3px black";
+link3.style.border = "3px solid navy";
+link3.style.borderRadius = "5px";
+link3.style.fontSize = "1.9vw";
+link3.style.fontWeight = "bold";
+
+link3.addEventListener("mouseover", function() {
+  this.style.backgroundColor = "orange";
+  this.style.border = "3px solid orange";
+  this.style.borderRadius = "20px";
+  this.style.transition = "1s";
+});
+
+link3.addEventListener("mouseout", function() {
+  this.style.backgroundColor = "navy";
+  this.style.border = "3px solid navy";
+  this.style.borderRadius = "5px";
+  this.style.transition = "1s";
+});
+
+var link4 = document.querySelector(".link4");
+
+link4.style.backgroundColor = "navy";
+link4.style.boxShadow = "3px 3px 3px black";
+link4.style.padding = "5px";
+link4.style.textAlign = "justify";
+link4.style.color = "white";
+link4.style.textShadow = " 1px 1px 3px black";
+link4.style.border = "3px solid navy";
+link4.style.borderRadius = "5px";
+link4.style.fontSize = "1.9vw";
+link4.style.fontWeight = "bold";
+
+link4.addEventListener("mouseover", function() {
+  this.style.backgroundColor = "orange";
+  this.style.border = "3px solid orange";
+  this.style.borderRadius = "20px";
+  this.style.transition = "1s";
+});
+
+link4.addEventListener("mouseout", function() {
+  this.style.backgroundColor = "navy";
+  this.style.border = "3px solid navy";
+  this.style.borderRadius = "5px";
+  this.style.transition = "1s";
+});
+
+var link5 = document.querySelector(".link5");
+
+link5.style.backgroundColor = "navy";
+link5.style.boxShadow = "3px 3px 3px black";
+link5.style.padding = "5px";
+link5.style.textAlign = "justify";
+link5.style.color = "white";
+link5.style.textShadow = " 1px 1px 3px black";
+link5.style.border = "3px solid navy";
+link5.style.borderRadius = "5px";
+link5.style.fontSize = "1.9vw";
+link5.style.fontWeight = "bold";
+
+link5.addEventListener("mouseover", function() {
+  this.style.backgroundColor = "orange";
+  this.style.border = "3px solid orange";
+  this.style.borderRadius = "20px";
+  this.style.transition = "1s";
+});
+
+link5.addEventListener("mouseout", function() {
+  this.style.backgroundColor = "navy";
+  this.style.border = "3px solid navy";
+  this.style.borderRadius = "5px";
+  this.style.transition = "1s";
+});
+
+var link6 = document.querySelector(".link6");
+
+link6.style.backgroundColor = "navy";
+link6.style.boxShadow = "3px 3px 3px black";
+link6.style.padding = "5px";
+link6.style.textAlign = "justify";
+link6.style.color = "white";
+link6.style.textShadow = " 1px 1px 3px black";
+link6.style.border = "3px solid navy";
+link6.style.borderRadius = "5px";
+link6.style.fontSize = "1.9vw";
+link6.style.fontWeight = "bold";
+
+link6.addEventListener("mouseover", function() {
+  this.style.backgroundColor = "orange";
+  this.style.border = "3px solid orange";
+  this.style.borderRadius = "20px";
+  this.style.transition = "1s";
+});
+
+link6.addEventListener("mouseout", function() {
+  this.style.backgroundColor = "navy";
+  this.style.border = "3px solid navy";
+  this.style.borderRadius = "5px";
+  this.style.transition = "1s";
+});
+
+
+var link00 = document.querySelector(".link00");
+
+link00.style.backgroundColor = "navy";
+link00.style.boxShadow = "3px 3px 3px black";
+link00.style.padding = "5px";
+link00.style.textAlign = "justify";
+link00.style.color = "white";
+link00.style.textShadow = " 1px 1px 3px black";
+link00.style.border = "3px solid navy";
+link00.style.borderRadius = "5px";
+link00.style.fontSize = "1.9vw";
+link00.style.fontWeight = "bold";
+
+link00.addEventListener("mouseover", function() {
+  this.style.backgroundColor = "orange";
+  this.style.border = "3px solid orange";
+  this.style.borderRadius = "20px";
+  this.style.transition = "1s";
+});
+
+link00.addEventListener("mouseout", function() {
+  this.style.backgroundColor = "navy";
+  this.style.border = "3px solid navy";
+  this.style.borderRadius = "5px";
+  this.style.transition = "1s";
+});
+
+var link0 = document.querySelector(".link0");
+
+link0.style.backgroundColor = "navy";
+link0.style.boxShadow = "3px 3px 3px black";
+link0.style.padding = "5px";
+link0.style.textAlign = "justify";
+link0.style.color = "white";
+link0.style.textShadow = " 1px 1px 3px black";
+link0.style.border = "3px solid navy";
+link0.style.borderRadius = "5px";
+link0.style.fontSize = "1.9vw";
+link0.style.fontWeight = "bold";
+
+link0.addEventListener("mouseover", function() {
+  this.style.backgroundColor = "orange";
+  this.style.border = "3px solid orange";
+  this.style.borderRadius = "20px";
+  this.style.transition = "1s";
+});
+
+link0.addEventListener("mouseout", function() {
+  this.style.backgroundColor = "navy";
+  this.style.border = "3px solid navy";
+  this.style.borderRadius = "5px";
+  this.style.transition = "1s";
+});
